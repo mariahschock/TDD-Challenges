53ba26287c508dc1f8d8372827941486c8cba8e0
Mariah <mariahschock@gmail.com>
2022-05-03 10:30:50 -0700

    challenge 1

--- test/tests.js
+++ test/tests.js
@@ -1,1 +1,8 @@
 import './functions.test.js';
+
+const { test } = QUnit;
+test('this test should pass', (expect) => {
+    const expected = 'bunny rabbit!!!';
+    const actual = myFunction('bunny rabbit');
+    expect.equal(actual, expected, 'bunny rabbit');
+});
